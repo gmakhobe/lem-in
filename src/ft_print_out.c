530d951e68570370b2e9c67fe6e3704f3a04df6e
Mbukutshe <wisemanmbukutshe@gmail.com>
2020-05-13 16:04:11 +0200

    printing results functions

--- src/ft_print_out.c
+++ src/ft_print_out.c
@@ -14,6 +14,22 @@ void        ft_print_point(t_list *list)
     t_turn  *point;
 
     point = (t_turn*)list->content;
+    ft_putstr(ft_ant_link(ft_itoa(point->ant->id), point->room->name));
     (point->last_move)? ft_putchar('\n') : ft_putchar(' ');
+    return ;
+}
+
+char        *ft_ant_link(char *id, char *name)
+{
+    char    *str;
+    char    *s;
+
+    str = ft_strjoin(id, ft_strjoin("-", name));
+    s = ft_strjoin("L", str);
+    return (s);
+}
+
+void        ft_print_out(t_list *value, t_list *points)
+{
     return ;
 }
