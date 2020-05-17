3701a2a322bb1e1956ffd6c7ff4131221854cc35
Thabang <thabangch@bbd.co.za>
2020-05-17 20:05:49 +0200

    remove space

--- src/ft_connections_to_the_rooms.c
+++ src/ft_connections_to_the_rooms.c
@@ -3,10 +3,10 @@ int		ft_connections_to_the_rooms(t_list *input, t_farm *farm,
 							t_room **from, t_room **to) //set_links_rooms function
 {
 	char	**connector;
+
     connector = ft_strsplit((char*)(input->content), '-');
     *from = ft_room_name(farm->rooms, connector[0]);
     *to = ft_room_name(farm->rooms, connector[1]);
-
 	if (!connector)
 		return (0);
 	if (!(*from) || !(*to) || (*from) == (*to))
