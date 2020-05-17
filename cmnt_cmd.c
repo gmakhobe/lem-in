2fd7832617ab11d9130f12e073d56df59bc2e176
samofoke <samofoke@student.wethinkcode.co.za>
2020-05-17 22:54:58 +0200

    cmd function

--- cmnt_cmd.c
+++ cmnt_cmd.c
@@ -6,10 +6,21 @@
 /*   By: samofoke <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/17 20:52:19 by samofoke          #+#    #+#             */
-/*   Updated: 2020/05/17 20:52:39 by samofoke         ###   ########.fr       */
+/*   Updated: 2020/05/17 22:54:33 by samofoke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
+int     cmd(t_list **data)
+{
+    int res;
 
+    if ((res = get_cmd(*data)) != -1 && 
+            res != START_COMMAND && res != END_COMMAND)
+    {
+        *data = (*data)->next;
+        return (1);
+    }
+    return (0);
+}
