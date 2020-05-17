abb59d33f847eb07d7b7ac1f6a1a88268bae9964
samofoke <samofoke@student.wethinkcode.co.za>
2020-05-17 23:34:30 +0200

    resolve rooms

--- res_room.c
+++ res_room.c
@@ -6,7 +6,7 @@
 /*   By: samofoke <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/17 23:10:24 by samofoke          #+#    #+#             */
-/*   Updated: 2020/05/17 23:21:06 by samofoke         ###   ########.fr       */
+/*   Updated: 2020/05/17 23:33:50 by samofoke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,4 +23,29 @@ static int      add_rooms(t_room *r, t_farm *f)
     }
     ft_lstappend(&(f->rooms), n);
     return (1);
+}
+
+int			resolve_room(t_list **data, t_farm *f)
+{
+    int cmd;
+    t_room  *r;
+
+    if ((cmd = get_cmd(*data)) != -1 && 
+            ((cmd == START_COMMAND && !(f->start)) || 
+             (cmd == END_COMMAND && !(f->end))))
+        *data = (*data)->next;
+    if (is_link(*data))
+        return (1);
+	if (!(*data) || !(r = resolve_info((char*)((*data)->content)))
+		|| !add_to_rooms(r, f) ||
+		(cmd == START_COMMAND && f->start) ||
+		(cmd == END_COMMAND && f->end))
+		return (0);
+	if (cmd == START_COMMAND && !(f->start))
+		f->start = r;
+	else if (cmd == END_COMMAND && !(f->end))
+		f->end = r;
+	if (find_info(f, r))
+		return (0);
+	return (1);
 }
