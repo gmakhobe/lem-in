ff731b84931f947fd76a28736cb1f29cb6630446
Thabang <thabangch@bbd.co.za>
2020-05-17 18:56:22 +0200

    added the board

---
+++ src/ft_board_Size.c
@@ -0,0 +1,12 @@
+size_t		ft_board_size(char **board)//get_table_size
+{
+	size_t i;
+
+	i = 0;
+    if(!board)
+        return 0;
+    else
+       while (board[i] != NULL)
+			i++;
+	return (i);
+}
