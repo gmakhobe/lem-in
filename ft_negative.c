1c105e4063000b736a009f2e15650881588e3f8d
samofoke <samofoke@student.wethinkcode.co.za>
2020-05-18 14:10:10 +0200

    check3

---
+++ ft_negative.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_negative.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: samofoke <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/18 13:47:23 by samofoke          #+#    #+#             */
+/*   Updated: 2020/05/18 13:50:46 by samofoke         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lem_in.h"
+
+int		ft_atoi_n(const char *str, size_t n)
+{
+	unsigned int	nbr;
+	int				neg;
+
+	while (ft_space(*str))
+		str++;
+	if (*str == '-')
+	{
+		neg = 1;
+		str++;
+	}
+	else
+	{
+		neg = 0;
+		if (*str == '+')
+			str++;
+	}
+	nbr = 0;
+	while (ft_isdigit(*str) && n--)
+	{
+		nbr = nbr * 10 + (*str - '0');
+		str++;
+	}
+	return (neg) ? -(int)nbr : (int)nbr;
+}
